23dda20fbd64df76dbf308ae9ef4ebeb81c82c08
bsknishanth <bsknishanth@gmail.com>
2017-01-14 16:40:33 -0500

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+export default App extends React.Compnent {
+    render(){
+        return (
+        <div>
+            <h1>React ToDos App</h1>
+            </div>
+        )
+    }
+}
